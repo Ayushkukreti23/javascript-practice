d20cddffb0b4f8df8a2aa4be41855c238552b471
Ayushkukreti23 <ayushkukreti882@gmail.com>
2023-11-22 22:05:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn, markHigherBMI);
+if (BMIMark > BMIJohn) {
+  console.log(``);
+}
